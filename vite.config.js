bc50d30c06b194c4441cb1ff73f2b8bdf35a83ca
JuanDiegoSalinas1708 <juandiegosalinascontreras.17@gmail.com>
2025-10-24 15:14:50 -0500

    Setup Vite + GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/landing_cgmlti/'  // <- nombre exacto de tu repo
+  base: '/landing_cgmlti/' // <- nombre exacto de tu repo en GitHub
 })
-
