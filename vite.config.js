fc408f73f19051277d0ed09254284348675377b8
JuanDiegoSalinas1708 <134516091+JuanDiegoSalinas1708@users.noreply.github.com>
2025-10-24 14:46:08 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/landing_cgmlti/' 
+  base: '/landing_cgmlti/'  // <- nombre exacto de tu repo
 })
+
